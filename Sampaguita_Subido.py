ea798687092ac7c3333b2921b2ede7a79120a484
Nathalie-Subido <nbsubido@carc.pshs.edu.ph>
2026-08-13 08:24:38 +0800

    Add files via upload

---
+++ Sampaguita_Subido.py
@@ -0,0 +1,10 @@
+#=======================================================================================================================
+#Programmer: Nathalie Kate B. Subido
+#Date: 08/13/2026
+#Section: 8-Sampaguita
+#Description: This is my first activity on Pycharm in SY 2026-2027. It introduces my name and my nickname
+#=======================================================================================================================
+
+print("""Hello Everyone!
+
+My name is Nathalie Subido. You can call me Nat for short:>!""")
